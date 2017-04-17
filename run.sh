afba59084289e05380f5aa04edecf78f28d98676
Chris Wilks <cwilks3@jhu.edu@login-node03.cm.cluster>
2017-04-17 14:22:01 -0400

    added base counting via Perl to run script

--- run.sh
+++ run.sh
@@ -6,6 +6,10 @@ INPUT1=../tests/$INPUT
 K=21
 K_sq=27
 
+
+#prog=perl
+#./syrupy.py --command-in-front --show-command --no-raw-process-log --syrupy-in-front --separator=, --no-align -i 1 perl ./count_kmers.pl tests/${INPUT} > ../runs/${date}.${INPUT}.${K}.1.$prog 2>../runs/${date}.${INPUT}.${K}.2.$prog
+
 prog=kmc
 cd kmc3 && ../syrupy.py --command-in-front --show-command --no-raw-process-log --syrupy-in-front --separator=, --no-align -i 1 ./kmc -k$K -m900 $INPUT1 ${INPUT1}.kmc3 ./ > ../runs/${date}.${INPUT}.${K}.1.$prog 2>../runs/${date}.${INPUT}.${K}.2.$prog
 cd ..
