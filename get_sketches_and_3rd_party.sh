3e2031def67721390bccd87fd2ef3d058cd7f4e3
ChristopherWilks <broadsword@gmail.com>
2017-05-13 17:08:25 -0400

    added my forks of libbf and squeakr

--- get_sketches_and_3rd_party.sh
+++ get_sketches_and_3rd_party.sh
@@ -1,23 +1,35 @@
 #!/bin/bash
 
 git clone -b cml https://github.com/ChristopherWilks/Lighter.git ./lighter_cml
-git clone -b bf2 https://github.com/ChristopherWilks/Lighter.git ./ligther_bf2
-git clone -b cqf https://github.com/ChristopherWilks/Lighter.git ./ligther_cqf
-git clone -b sf_sketch https://github.com/ChristopherWilks/Lighter.git ./ligther_sf
-git clone -b sf_sketch https://github.com/ChristopherWilks/squeakr.git ./squeakr
+git clone https://github.com/ChristopherWilks/libbf.git ./libbf
+git clone -b bf2 https://github.com/ChristopherWilks/Lighter.git ./lighter_bf2
+git clone -b cqf https://github.com/ChristopherWilks/Lighter.git ./lighter_cqf
+git clone -b sf_sketch https://github.com/ChristopherWilks/Lighter.git ./lighter_sf
+git clone https://github.com/ChristopherWilks/squeakr.git ./squeakr
 
-for i in lighter_cml ligther_cqf ligther_sf
+for i in lighter_cml lighter_cqf lighter_sf
 do
 	cd $i && make clean && make
 	cd ..
 done
 
 wget https://github.com/refresh-bio/KMC/releases/download/v3.0.0/KMC3.linux.tar.gz
-gunzip -zxvf KMC3.linux.tar.gz
+mkdir kmc
+cd kmc
+tar -zxvf ../KMC3.linux.tar.gz
+cd ..
+
+source libbf/env.sh
+cd libbf
+./configure && make
+cd ..
 
 source lighter_bf2/env.sh
-cd lighter_bf2 && make clean && make
+cd lighter_bf2
+ln -fs ../libbf/build/src/bf/libbf.so && make clean && make
 cd ..
 
 source squeakr/env.sh
-cd squeakr && make clean && make
+cd squeakr
+make clean && make
+cd ..
