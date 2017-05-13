a196801383c4250a1c14ef3360e1f69835640e9e
ChristopherWilks <broadsword@gmail.com>
2017-05-13 15:55:53 -0400

    added mkdirs of tests and runs

--- get_data.sh
+++ get_data.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 #download larger data sets for simple K-mer counting testing of torch
 
+mkdir ./tests
+mkdir ./runs
 for f in SRR197986_1.fastq.1m SRR197986_1.fastq.1m.kmers.gz.rand1k SRR197986_1.fastq.1m.kmers.gz.rand1k.out SRR197986_1.fastq.2m SRR197986_1.fastq.2m.kmers.gz.rand1k SRR197986_1.fastq.2m.kmers.gz.rand1k.out SRR197986_1.fastq.4m SRR197986_1.fastq.4m.kmers.gz.rand1k SRR197986_1.fastq.4m.kmers.gz.rand1k.out ecoli_reads_140_1.fq
 do
 	wget http://snaptron.cs.jhu.edu/$f -O tests/$f
