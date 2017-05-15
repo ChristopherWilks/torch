dd4040fa7418dd009727a57b37746af47db4ea9b
Chris Wilks <broadsword@gmail.com>
2017-05-15 19:21:06 -0400

    adding Rcorrector data & repos

--- get_data.sh
+++ get_data.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 #download larger data sets for simple K-mer counting testing of torch
 
-mkdir ./tests
+mkdir -p ./tests/simulate
 mkdir ./runs
-for f in SRR197986_1.fastq.1m SRR197986_1.fastq.1m.kmers.gz.rand1k SRR197986_1.fastq.1m.kmers.gz.rand1k.out SRR197986_1.fastq.2m SRR197986_1.fastq.2m.kmers.gz.rand1k SRR197986_1.fastq.2m.kmers.gz.rand1k.out SRR197986_1.fastq.4m SRR197986_1.fastq.4m.kmers.gz.rand1k SRR197986_1.fastq.4m.kmers.gz.rand1k.out ecoli_reads_140_1.fq
+for f in SRR197986_1.fastq.1m SRR197986_1.fastq.1m.kmers.gz.rand1k SRR197986_1.fastq.1m.kmers.gz.rand1k.out SRR197986_1.fastq.2m SRR197986_1.fastq.2m.kmers.gz.rand1k SRR197986_1.fastq.2m.kmers.gz.rand1k.out SRR197986_1.fastq.4m SRR197986_1.fastq.4m.kmers.gz.rand1k SRR197986_1.fastq.4m.kmers.gz.rand1k.out ecoli_reads_140_1.fq ecoli_reads_140_1.fq.21.full.kmers.gz.rand1k ecoli_reads_140_1.fq.21.full.kmers.gz.rand1k.out sim_chr20_read1.fq.gz sim_chr20_read2.fq.gz
 do
 	wget http://snaptron.cs.jhu.edu/$f -O tests/$f
 done
+
+mv tests/sim_chr20_read?.fq.gz tests/simulate/
--- get_sketches_and_3rd_party.sh
+++ get_sketches_and_3rd_party.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+#unpack/git clone relevant branches from Lighter and Rcorrector and build
+#as well as KMC3 and Squeakr
 
 git clone -b cml https://github.com/ChristopherWilks/Lighter.git ./lighter_cml
 git clone https://github.com/ChristopherWilks/libbf.git ./libbf
@@ -7,18 +9,18 @@ git clone -b cqf https://github.com/ChristopherWilks/Lighter.git ./lighter_cqf
 git clone -b sf_sketch https://github.com/ChristopherWilks/Lighter.git ./lighter_sf
 git clone https://github.com/ChristopherWilks/squeakr.git ./squeakr
 
-for i in lighter_cml lighter_cqf lighter_sf
+git clone https://github.com/ChristopherWilks/Rcorrector.git ./Rcorrector_original
+git clone -b cqf https://github.com/ChristopherWilks/Rcorrector.git ./Rcorrector_cqf
+git clone -b cml https://github.com/ChristopherWilks/Rcorrector.git ./Rcorrector_cml
+git clone -b bf2 https://github.com/ChristopherWilks/Rcorrector.git ./Rcorrector_bf2
+
+for i in lighter_cml lighter_cqf lighter_sf Rcorrector_original Rcorrector_cml Rcorrector_cqf
 do
 	cd $i && make clean && make
 	cd ..
 done
 
-wget https://github.com/refresh-bio/KMC/releases/download/v3.0.0/KMC3.linux.tar.gz
-mkdir kmc
-cd kmc
-tar -zxvf ../KMC3.linux.tar.gz
-cd ..
-
+#special library needed for BF2 (TOMB) for counting BF
 source libbf/env.sh
 cd libbf
 ./configure && make
@@ -27,6 +29,17 @@ cd ..
 source lighter_bf2/env.sh
 cd lighter_bf2
 ln -fs ../libbf/build/src/bf/libbf.so && make clean && make
+cd ..
+
+cd Rcorrector_bf2
+ln -fs ../libbf/build/src/bf/libbf.so && make clean && make
+cd ..
+
+#now 3rd party
+wget https://github.com/refresh-bio/KMC/releases/download/v3.0.0/KMC3.linux.tar.gz
+mkdir kmc
+cd kmc
+tar -zxvf ../KMC3.linux.tar.gz
 cd ..
 
 source squeakr/env.sh
