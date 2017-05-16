1f99a1e93b8026e9c0bd97665c8414b2c6ef25b9
ChristopherWilks <broadsword@gmail.com>
2017-05-16 19:01:28 -0400

    removed simulate subdir

--- get_data.sh
+++ get_data.sh
@@ -7,5 +7,3 @@ for f in SRR197986_1.fastq.1m SRR197986_1.fastq.1m.kmers.gz.rand1k SRR197986_1.f
 do
 	wget http://snaptron.cs.jhu.edu/$f -O tests/$f
 done
-
-mv tests/sim_chr20_read?.fq.gz tests/simulate/
--- run_rcorrector_tests.sh
+++ run_rcorrector_tests.sh
@@ -8,9 +8,9 @@ rm tmp_ee5818769bb7b8219347f982bed69089.bc
 rm tmp_ee5818769bb7b8219347f982bed69089.mer_counts
 
 #do initial JellyFish2 counting
-./Rcorrector/jellyfish/bin/jellyfish bc -m 23 -s 100000000 -C -t 1 -o tmp_ee5818769bb7b8219347f982bed69089.bc <(gzip -cd ./tests/simulate/sim_chr20_read1.fq.gz) <(gzip -cd ./tests/simulate/sim_chr20_read2.fq.gz)
+./Rcorrector/jellyfish/bin/jellyfish bc -m 23 -s 100000000 -C -t 1 -o tmp_ee5818769bb7b8219347f982bed69089.bc <(gzip -cd ./tests/sim_chr20_read1.fq.gz) <(gzip -cd ./tests/sim_chr20_read2.fq.gz)
 
-./Rcorrector/jellyfish/bin/jellyfish count -m 23 -s 100000 -C -t 1 --bc tmp_ee5818769bb7b8219347f982bed69089.bc -o tmp_ee5818769bb7b8219347f982bed69089.mer_counts <(gzip -cd ./tests/simulate/sim_chr20_read1.fq.gz) <(gzip -cd ./tests/simulate/sim_chr20_read2.fq.gz)
+./Rcorrector/jellyfish/bin/jellyfish count -m 23 -s 100000 -C -t 1 --bc tmp_ee5818769bb7b8219347f982bed69089.bc -o tmp_ee5818769bb7b8219347f982bed69089.mer_counts <(gzip -cd ./tests/sim_chr20_read1.fq.gz) <(gzip -cd ./tests/sim_chr20_read2.fq.gz)
 
 ./Rcorrector/jellyfish/bin/jellyfish dump -L 2 tmp_ee5818769bb7b8219347f982bed69089.mer_counts > tmp_ee5818769bb7b8219347f982bed69089.jf_dump
 
@@ -21,7 +21,7 @@ do
 	rm sim_chr20_read?.cor.fq.gz
 
 	#run actual rcorrector code
-	../syrupy.py --command-in-front --show-command --no-raw-process-log --syrupy-in-front --separator=, --no-align -i 1 ./rcorrector -t 1 -p ../tests/simulate/sim_chr20_read1.fq.gz ../tests/simulate/sim_chr20_read2.fq.gz -k 23 -c ../tmp_ee5818769bb7b8219347f982bed69089.jf_dump > sim_chr20_run.sweep.1.only 2> sim_chr20_run.sweep.2.only
+	../syrupy.py --command-in-front --show-command --no-raw-process-log --syrupy-in-front --separator=, --no-align -i 1 ./rcorrector -t 1 -p ../tests/sim_chr20_read1.fq.gz ../tests/sim_chr20_read2.fq.gz -k 23 -c ../tmp_ee5818769bb7b8219347f982bed69089.jf_dump > sim_chr20_run.sweep.1.only 2> sim_chr20_run.sweep.2.only
 
 	#time stat
 	grep "Total run time" sim_chr20_run.sweep.2.only | perl -ne 'chomp; $f='${i}'; $f=~s/Rcorrector_//; $s=$_; $s=~/Total run time.+ (\d+) hour.+ (\d+) minute.+ (\d+\.\d+) second\(s\)$/; $run_time=$3; $run_time += (60*$2); $run_time += (60*60*$1); printf("%s\t%.0f",$f,$run_time);' >> ../rcorrector_results.tsv
